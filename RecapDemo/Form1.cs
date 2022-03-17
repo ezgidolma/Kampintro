d1ab0dcfd30351989d484fbaaae79082ff965371
ezgi dolma <ezgilearniy@gmail.com>
2022-03-17 17:35:09 +0300

    Son ders

---
+++ RecapDemo/Form1.cs
@@ -0,0 +1,48 @@
+namespace RecapDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Button button = new Button();
+            //button.Width = 50;
+            //button.Height = 50;
+            //button.Text = "My button";
+            //this.Controls.Add(button);//butonu ekrana koy demek
+
+            Button[,] buttons = new Button[8, 8];
+            int top=0;
+            int left=0;
+
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Left = left;
+                    left += 50;
+                    buttons[i, j].Top = top;
+                    if ((i+j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.Red;
+                    }
+                    this.Controls.Add(buttons[i,j]);
+                }
+                top += 50;
+                left = 0;
+                
+            }
+        }
+    }
+}
